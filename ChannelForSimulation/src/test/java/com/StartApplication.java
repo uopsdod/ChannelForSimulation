87bffc8c669d0dbabf1657b859efea5b40f58ff8
SamTsai <uopsdod@gmail.com>
2017-09-07 18:13:55 +0800

    #TASK - 調整測試架構

--- ChannelForSimulation/src/test/java/com/StartApplication.java
+++ ChannelForSimulation/src/test/java/com/StartApplication.java
@@ -1,5 +1,5 @@
 
-package com.test;
+package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- ChannelForSimulation/src/test/java/com/client/LoginTest.java
+++ ChannelForSimulation/src/test/java/com/client/LoginTest.java
@@ -1,4 +1,4 @@
-package com.test;
+package com.client;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,6 +14,7 @@ import com.util.TestUtil;
 import com.util.Util;
 
 @RunWith(SpringRunner.class)
+
 @SpringBootTest
 public class LoginTest {
 	@Test
